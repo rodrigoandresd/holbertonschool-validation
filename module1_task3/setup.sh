296e88dec10283ffbdecda83fa71a5842bf3ce65
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-09 21:29:35 -0500

    task 3 3. From Development to Production

---
+++ module1_task3/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install required packages
+apt-get update
+apt-get install -y make golang hugo
+
+# Build the website
+make build
+
+# If the build fails, print the error message and exit with the appropriate error code
+if [ $? -ne 0 ]; then
+  make build 2>&1 | tee /dev/stderr
+  exit $?
+fi
+
+# If the build succeeds, exit with a success code
+exit 0
