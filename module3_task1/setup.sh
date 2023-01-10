21c866d18e84b3b256a15218411418a1a99bf963
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-10 15:06:26 -0500

    task 1 Add Build Dependencies to the CI Env.6

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -5,4 +5,3 @@ apt-get update && apt-get install -y curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 apt-get install -y ./hugo.deb
 rm hugo.deb
-apt-get install -y make
