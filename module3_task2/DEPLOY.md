487a4d1830f57329cca00d5c67e49fa887c3f4cb
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-10 17:28:43 -0500

    Task 2 2. Earlier Feedback with CD

---
+++ module3_task2/DEPLOY.md
@@ -0,0 +1,9 @@
+# awesome-website.zip
+
+## In the archive
+
+## To start and stop the application
+
+## How to customize where the application logs are written?
+
+## How to “quickly” verify that the application is running (healthcheck)?
---
+++ module3_task2/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install required packages
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+rm -f hugo_extended_0.109.0_linux-amd64.deb
