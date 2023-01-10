de253506489e4bb7234948a0daa281522ba11404
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-09 21:43:14 -0500

    task 3. From Development to Production3

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
 # Install required packages
-apt-get update
-apt-get install -y make hugo make
+apt-get update && apt-get install -y hugo make
 
 # Build the website
 make build
-
-# If the build fails, print the error message and exit with the appropriate error code
-if [ $? -ne 0 ]; then
-  make build 2>&1 | tee /dev/stderr
-  exit $?
-fi
-
-# If the build succeeds, exit with a success code
-exit 0
