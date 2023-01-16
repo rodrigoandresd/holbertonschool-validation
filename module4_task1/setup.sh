5b09ad1056a2cdffa6f4034bb0af4b142b462997
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-16 17:34:06 -0500

    task1. Docker Image + golangci-lint

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -8,4 +8,6 @@ rm -f hugo_extended_0.109.0_linux-amd64.deb
 # Install markdownlint and zip tools
 apt-get install zip
 npm install -g markdownlint-cli
+# Install golangci-lint
+curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.32.2
 rm -rf /usr/local/go
