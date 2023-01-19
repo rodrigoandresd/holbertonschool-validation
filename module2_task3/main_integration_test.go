c0156dbc385f1092a4c5d7e3b578a1255aa99e47
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-01-19 14:00:09 -0500

    3. Integration Tests- Golang.3 test

--- module2_task3/main_integration_test.go
+++ module2_task3/main_integration_test.go
@@ -1,87 +1,96 @@
 package main
 
 import (
-  "io/ioutil"
-  "net/http"
-  "net/http/httptest"
-  "testing"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func Test_server(t *testing.T) {
-  if testing.Short() {
-    t.Skip("Flag `-short` provided: skipping Integration Tests.")
-  }
+	if testing.Short() {
+		t.Skip("Flag `-short` provided: skipping Integration Tests.")
+	}
 
-  tests := []struct {
-    name         string
-    URI          string
-    responseCode int
-    body         string
-  }{
-    {
-      name:         "Home page",
-      URI:          "",
-      responseCode: 404,
-      body:         "404 page not found\n",
-    },
-    {
-      name:         "Health page",
-      URI:          "/hello?name=Holberton",
-      responseCode: 200,
-      body:         "Hello Holberton!",
-      {
-        name:         "Name no value",
-        queryString:  "name",
-        responseCode: 400,
-      },
-      {
-        name:         "Name Empty",
-        queryString:  "name=",
-        responseCode: 400,
-      },
-      {
-        name:         "Another queryString halo",
-        queryString:  "halo=Haloooo!",
-        responseCode: 200,
-        body:         "Hello !",
-      },
-      {
-        name:         "Without any param",
-        queryString:  "name=test&name=lol",
-        responseCode: 200,
-        body:         "Hello lol!",
-      },
-    },
-  }
+	tests := []struct {
+		name         string
+		URI          string
+		responseCode int
+		body         string
+	}{
+		{
+			name:         "Home page",
+			URI:          "",
+			responseCode: 404,
+			body:         "404 page not found\n",
+		},
+		{
+			name:         "Health page",
+			URI:          "/hello?name=Holberton",
+			responseCode: 200,
+			body:         "Hello Holberton!",
+		},
+		// new
+		{
+			name:         "Health page2",
+			URI:          "/health",
+			responseCode: 200,
+			body:         "ALIVE",
+		},
+		{
+			name:         "Undefined page",
+			URI:          "/test",
+			responseCode: 404,
+			body:         "404 page not found\n",
+		},
+		{
+			name:         "Undefined name",
+			URI:          "/hello?name",
+			responseCode: 200,
+			body:         "Hello there!",
+		},
+		{
+			name:         "Another queryString halo",
+			URI:          "/hello?halo=Haloooo!",
+			responseCode: 200,
+			body:         "Hello there!",
+		},
+		{
+			name:         "Without any param",
+			URI:          "/hello?name=test&name=lol",
+			responseCode: 200,
+			body:         "Hello lol!",
+		},
+	}
 
-  for _, tt := range tests {
-    t.Run(tt.name, func(t *testing.T) {
-      ts := httptest.NewServer(setupRouter())
-      defer ts.Close()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(setupRouter())
+			defer ts.Close()
 
-      res, err := http.Get(ts.URL + tt.URI)
-      if err != nil {
-        t.Fatal(err)
-      }
+			res, err := http.Get(ts.URL + tt.URI)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-      // Check that the status code is what you expect.
-      expectedCode := tt.responseCode
-      gotCode := res.StatusCode
-      if gotCode != expectedCode {
-        t.Errorf("handler returned wrong status code: got %q want %q", gotCode, expectedCode)
-      }
+			// Check that the status code is what you expect.
+			expectedCode := tt.responseCode
+			gotCode := res.StatusCode
+			if gotCode != expectedCode {
+				t.Errorf("handler returned wrong status code: got %q want %q", gotCode, expectedCode)
+			}
 
-      // Check that the response body is what you expect.
-      expectedBody := tt.body
-      bodyBytes, err := ioutil.ReadAll(res.Body)
-      res.Body.Close()
-      if err != nil {
-        t.Fatal(err)
-      }
-      gotBody := string(bodyBytes)
-      if gotBody != expectedBody {
-        t.Errorf("handler returned unexpected body: got %q want %q", gotBody, expectedBody)
-      }
-    })
-  }
+			// Check that the response body is what you expect.
+			expectedBody := tt.body
+			bodyBytes, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+			gotBody := string(bodyBytes)
+			if gotBody != expectedBody {
+				t.Errorf("handler returned unexpected body: got %q want %q", gotBody, expectedBody)
+			}
+		})
+	}
 }
